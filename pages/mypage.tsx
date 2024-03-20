76f5e3ece5f1480a9f11d5896baeebd4b16102a2
Eomhyunjun <zzp1318@gmail.com>
2024-03-20 14:10:03 +0900

    💄 메인 페이지 스타일 개선

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,16 +1,15 @@
-import MyInfo from "../components/myinfo";
-import PostGroup from "../components/post-group";
+import MyInfo from '../components/myinfo'
+import PostGroup from '../components/post-group'
 
 export default function ProfileChange() {
-
   return (
-    <div className="py-[18px]">
-      <section className="mb-[-203px] mx-auto w-base flex flex-row items-start pb-3 justify-start box-border gap-[45px] max-w-full shrink-0 text-left text-base text-black font-small-m">
+    <div className="w-full py-[18px]">
+      <section className="mb-[-203px] mx-auto flex flex-row justify-center items-start pb-3 justify-start box-border gap-[45px] shrink-0 text-left text-base text-black font-small-m">
         <MyInfo />
-        <div className="flex-1 flex flex-col items-start justify-start pt-1s box-border max-w-full ">
+        <div className="flex flex-col items-start justify-start pt-1s box-border ">
           <PostGroup />
         </div>
       </section>
     </div>
-  );
-};
+  )
+}
