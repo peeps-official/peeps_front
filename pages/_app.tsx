e04e3652ac7b32bc16551fed6b2115ba1f18ae63
Eomhyunjun <zzp1318@gmail.com>
2024-03-20 14:04:37 +0900

    💄  로그인 페이지 레이아웃 제작

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import { Fragment } from "react";
-import Head from "next/head";
-import type { AppProps } from "next/app";
-import "./global.css";
-import GlobalHeader from "../components/GlobalHeader"
+import { Fragment } from 'react'
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+import './global.css'
+import GlobalHeader from '../components/GlobalHeader'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  const hideHeader = router.pathname === '/login'
+  const headerPadding = hideHeader ? '' : 'pt-header bg-background'
   return (
     <>
       <Head>
@@ -13,13 +15,19 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <GlobalHeader />
-      <div className="pt-header bg-background">
+      {!hideHeader && <GlobalHeader />}
+      <div className={headerPadding}>
         <Component {...pageProps} />
       </div>
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
