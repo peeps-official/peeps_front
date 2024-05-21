90f2b0c45a8a5419c69bdabd8bc0191f74039d23
Eom Hyun Jun <zzp1318@gmail.com>
2024-05-21 15:01:02 +0900

    🚨 Invalid DOM property `crossorigin` 워닝 해결

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* google font */}
       <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
           rel="stylesheet"
