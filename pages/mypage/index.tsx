b0b5c2102dc6d4dd35a049f45433be61a0a64161
hye <hyekyung0610@naver.com>
2024-06-18 19:11:01 +0900

    💄 프로필 편집 팝업 추가

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -11,7 +11,7 @@ export default function ProfileChange() {
             {/* 프로필 */}
             <MyProfile />
             {/* 홈 & 피드 메뉴바 */}
-            <div className="self-stretch overflow-hidden flex flex-row items-start justify-start pt-0 px-[17px] pb-px gap-[24px] shrink-0 text-center text-xs font-inter border-b-[1px] border-solid border-gray-200/5 mq450:flex-wrap">
+            <div className="self-stretch overflow-hidden flex flex-row items-start justify-start pt-0 px-[17px] pb-px gap-[24px] shrink-0 text-center text-tiny font-inter font-bold border-solid border-gray-200 mq450:flex-wrap">
               <div className="flex flex-col items-start justify-start relative min-w-[48px]">
                 <div className="overflow-hidden flex flex-row items-start justify-start py-4 px-[18px]">
                   <b className="w-3 relative flex items-center justify-center min-w-[12px]">
@@ -21,8 +21,8 @@ export default function ProfileChange() {
                 <div className="w-full h-[3px] absolute !m-[0] right-[0px] bottom-[-2px] left-[0px] bg-black z-[1]" />
               </div>
               <div className="overflow-hidden flex flex-row items-start justify-start py-4 px-[18px] box-border min-w-[48px] text-dimgray-200">
-                <div className="flex flex-row items-start justify-start w-3">
-                  <b className="ml-[-5px] w-[23px] relative flex items-center justify-center min-w-[23px] shrink-0 [debug_commit:1de1738]">
+                <div className="flex flex-row items-start justify-start w-4">
+                  <b className="ml-[-5px] w-[26px] relative flex items-center justify-center min-w-[26px] shrink-0 [debug_commit:1de1738]">
                     피드
                   </b>
                 </div>
