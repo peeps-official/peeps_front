66678ae7392e20997911f481cd539a0ccbfc1142
Eom Hyun Jun <zzp1318@gmail.com>
2024-06-18 12:55:55 +0900

    [관리자페이지] 페이지 틀 잡기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,24 @@ import './global.css'
 import GlobalHeader from '../components/GlobalHeader'
 import GlobalSidebarLeft from '../components/GlobalSidebarLeft'
 
+import { Archivo } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
+import './styles.css'
+
+const archivo = Archivo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-archivo',
+})
+const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm_sans',
+})
+
 function MyApp({ Component, pageProps, router }: AppProps) {
-  const hideHeader = router.pathname === '/login'
+  const hideHeader =
+    router.pathname === '/login' || router.pathname === '/admin'
   return (
     <>
       <Head>
@@ -20,13 +36,27 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
-      {!hideHeader && <GlobalHeader />}
-      <main className={`self-stretch flex flex-row pt-[68px] ${hideHeader ? 'justify-center items-center' : ''}`}>
-          {!hideHeader && <GlobalSidebarLeft />}
-          <section className="flex-grow bg-white pl-[80px] overflow-auto">
-            <Component {...pageProps} />
-          </section>
-      </main>
+      {!hideHeader ? (
+        <>
+          <GlobalHeader />
+          <main
+            className={`self-stretch flex flex-row pt-[68px] ${
+              hideHeader ? 'justify-center items-center' : ''
+            }`}
+          >
+            <div className={`${archivo.variable + ' ' + dm_sans.variable}`}>
+              <GlobalSidebarLeft />
+              <section className="flex-grow bg-white pl-[80px] overflow-auto">
+                <Component {...pageProps} />
+              </section>
+            </div>
+          </main>
+        </>
+      ) : (
+        <div className={`${archivo.variable + ' ' + dm_sans.variable}`}>
+          <Component {...pageProps} />
+        </div>
+      )}
     </>
   )
 }
