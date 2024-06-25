c8f84c49b5e6ebc38b9f9d1c4c3682ef72ff1e82
Eom Hyun Jun <zzp1318@gmail.com>
2024-06-25 20:46:53 +0900

    ✨ admin 페이지 가입 user 테이블 기능 추가

---
+++ pages/admin/tableState.ts
@@ -0,0 +1,55 @@
+import { atom } from 'recoil'
+
+/**
+ * key = react-query와 recoil 공통 key
+ * url = fetch될 데이터의 url
+ * atom, fetch될 data type, tableHead 등 테이블과 관련된 상태를 관리하는 파일
+ */
+
+export const userDataKey: string = 'user'
+
+export const userDataUrl: string = 'http://localhost:9090/user/admin'
+
+export const uerData = atom<Array<UserData>>({
+  key: userDataKey,
+  default: [
+    {
+      id: '',
+      nickname: '',
+      profileImage: '',
+      backgroundImage: null,
+      info: null,
+      mobile: '',
+      name: '',
+      birthday: '',
+      birthyear: '',
+      boards: [],
+    },
+  ],
+})
+
+export interface UserData {
+  id: string
+  nickname: string
+  profileImage: string
+  backgroundImage: string | null
+  info: string | null
+  mobile: string
+  name: string
+  birthday: string
+  birthyear: string
+  boards: any[]
+}
+
+export const tableHead: (keyof UserData)[] = [
+  'id',
+  'nickname',
+  'profileImage',
+  'backgroundImage',
+  'info',
+  'mobile',
+  'name',
+  'birthday',
+  'birthyear',
+  'boards',
+]
