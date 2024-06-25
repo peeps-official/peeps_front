f30adf96a96c90309c3921b7699fb2ce23bea934
Eom Hyun Jun <zzp1318@gmail.com>
2024-06-25 16:50:20 +0900

    ⚰️ 필요없는 코드 제거

--- pages/login/callback.tsx
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from 'next/navigation'
-
-export default function Callback() {
-  const params = useSearchParams()
-
-  //여러개의 값을 한번에 가져오고 싶다면
-
-  return <div>하연 로그인</div>
-}
