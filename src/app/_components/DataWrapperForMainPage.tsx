bc953de445d127277c48e3ad99245a15147c7ba6
Eom Hyun Jun <zzp1318@gmail.com>
2024-08-06 17:33:04 +0900

    🚚 UserProfileData 삭제 및 폴더 이동

--- src/app/_components/DataWrapperForMainPage.tsx
+++ src/app/_components/DataWrapperForMainPage.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-import FetchUserProfileCom from '@/src/common/fetch/UserProfileData'
+import { fetchUserProfileData } from '@/src/common/api/mypage'
+import { uerDataStateAtom } from '@/src/common/recoil/userAtom'
+import { useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 
 interface DataWrapperForMainPageProps {
   children: React.ReactNode
@@ -9,5 +14,24 @@ interface DataWrapperForMainPageProps {
 export default function DataWrapperForMainPage({
   children,
 }: DataWrapperForMainPageProps) {
-  return <FetchUserProfileCom>{children}</FetchUserProfileCom>
+  const router = useRouter()
+  const [recoilData, setRecoilData] = useRecoilState(uerDataStateAtom)
+
+  const { isSuccess, error, data } = useQuery({
+    queryKey: ['login', 'mainPage'],
+    queryFn: () => fetchUserProfileData(),
+  })
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log('user: ', data)
+      setRecoilData(data)
+    }
+
+    if (error) {
+      router.push('/login')
+    }
+  }, [isSuccess, error])
+
+  return <>{children}</>
 }
