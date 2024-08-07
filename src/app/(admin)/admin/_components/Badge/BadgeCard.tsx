64017d0c0f4aeca51c5923123a5424abdc208bf3
Eom Hyun Jun <zzp1318@gmail.com>
2024-08-07 13:59:20 +0900

    [api] '/badge','/admin/badge' 연결 - 뱃지 리스팅, 생성

--- src/app/(admin)/admin/_components/Badge/BadgeCard.tsx
+++ src/app/(admin)/admin/_components/Badge/BadgeCard.tsx
@@ -1,16 +1,36 @@
 'use client'
 
+import { Badge_T } from '@/src/common/types/badge'
+import NextImg from '@/src/common/utils/NextImg'
+
 /**
  * 뱃지 카드 컴포넌트
  */
-export default function BadgeCard() {
+
+interface Props {
+  badge: Badge_T
+}
+
+export default function BadgeCard({ badge }: Props) {
+  const { name, image, content } = badge
+
   return (
-    <div>
-      <div>뱃지 이름</div>
-      <div>뱃지 이미지</div>
-      <div>뱃지 수정 버튼</div>
-      <div>뱃지 삭제 버튼</div>
-      <div>등록된 인증 방법 - ex.이메일 </div>
+    <div className="flex flex-col items-center justify-center max-w-sm p-5 overflow-hidden bg-white rounded shadow-lg ">
+      <div className="h-[100px] w-[100px] mb-[20px] shadow-xl rounded-[50%]">
+        <NextImg src={image} alt={name} />
+      </div>
+      <div className="mb-2 text-lg font-bold text-center">{name}</div>
+      <div className="mb-2 text-lg font-normal text-center text-gray-medium">
+        {content}
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+          수정
+        </button>
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+          삭제
+        </button>
+      </div>
     </div>
   )
 }
--- src/app/(admin)/admin/badge/page.tsx
+++ src/app/(admin)/admin/badge/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 /**
  * 뱃지 페이지 - 뱃지 자체 관리 페이지 (뱃지 생성, 수정, 삭제) / 참고: https://ccob.cccv.to/badge/list
  *
@@ -6,14 +7,55 @@
  * -> 뱃지 내부에 다양한 인증방법에 따른 내용 추가하는 방식으로 구현.
  */
 
+import { useMutation, useQueries, useQuery } from '@tanstack/react-query'
 import BadgeCard from '../_components/Badge/BadgeCard'
+import { axiosWithAuth } from '@/src/common/api/instance'
+import { Badge_T } from '@/src/common/types/admin'
 
-export default function page() {
+const tmp_data = [
+  {
+    name: 'test',
+    image: 'https://via.placeholder.com/150',
+    content: 'test content',
+  },
+]
+
+export default function AdminBadgePage() {
+  const { data: badgeData, isSuccess } = useQuery({
+    queryKey: ['admin', 'badge'],
+    queryFn: async () => await axiosWithAuth.get<Badge_T[]>('/badge'),
+  })
+  const { mutate } = useMutation({
+    mutationFn: async (data: Badge_T) =>
+      await axiosWithAuth.post('/admin/badge', data),
+    onSuccess: (variables) => {
+      console.log('success', variables)
+    },
+  })
+  console.log('badgeData', badgeData)
+  // const { data } = await axiosWithAuth.get<Badge_T[]>('/badge')
+
   return (
     <div className="p-5">
-      <h1>뱃지 관리</h1>
-      <button>뱃지추가 버튼</button>
-      <BadgeCard />
+      <h1 className="my-2">뱃지 관리</h1>
+      <button
+        className="p-3 my-4 rounded bg-blue-soft hover:bg-blue-secondary"
+        onClick={() => {
+          mutate({
+            name: '한경대학교',
+            image:
+              'https://i.namu.wiki/i/rlymHGzGkXgtwPzlj9jmLbyqGFcykXgKxesKDZ6bBtdVi9dsunCmoQzcSo7Yib6T7Y4rCbzxcZOZmsw-c89Fng.svg',
+            content: 'test content',
+          })
+        }}
+      >
+        뱃지추가 버튼
+      </button>
+      <div className="flex gap-10">
+        {badgeData?.data?.map((badge) => {
+          return <BadgeCard key={badge.name} badge={badge} />
+        })}
+      </div>
     </div>
   )
 }
