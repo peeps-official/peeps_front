4aad89fcf70c92aaf99e7e048335bddccf9cae6e
Eom Hyun Jun <zzp1318@gmail.com>
2024-08-19 16:49:07 +0900

    feat(@admin/badge/issue): 승인된 뱃지 리스트 테이블 추가 - (`/admin/verify/req/${id}`) api 연결 - 데이터 수정(뱃지 승인, 거절 등등) 후 refresh 기능 추가

--- src/app/(admin)/admin/badge/issue/page.tsx
+++ src/app/(admin)/admin/badge/issue/page.tsx
@@ -3,6 +3,13 @@ import { axiosWithAuth } from '@/src/common/api/instance'
 import { BadgeIssue_T } from '@/src/common/types/badge'
 import AdminTable from '../../_components/Table/AdminTable'
 import { useEffect, useState } from 'react'
+import { useQueries, useQueryClient } from '@tanstack/react-query'
+import {
+  ApproveBadge,
+  getBadgeApproveList,
+  getBadgeIssueList,
+  RejectBadge,
+} from '@/src/common/api/adminBadge'
 
 /**
  * 뱃지 발급 페이지 - 뱃지 발급 요청에 관한 페이지 (뱃지 발급 요청 승인, 거절)
@@ -13,18 +20,42 @@ import { useEffect, useState } from 'react'
  */
 export default function BadgeIssuePage() {
   const [badgeData, setBadgeData] = useState<BadgeIssue_T[] | null>(null)
+  const [approveData, setApproveData] = useState<BadgeIssue_T[] | null>(null)
+  const res = useQueries({
+    queries: [
+      {
+        queryKey: ['badgeData', 'issue'],
+        queryFn: getBadgeIssueList,
+      },
+      {
+        queryKey: ['badgeData', 'approve'],
+        queryFn: getBadgeApproveList,
+      },
+    ],
+  })
+  const queryClient = useQueryClient()
 
   useEffect(() => {
-    ;(async () => {
-      const { data } = await axiosWithAuth.get<BadgeIssue_T[]>('/admin/verify/req')
+    const [badgeDataRes, approveDataRes] = res
+    if (!badgeDataRes || badgeDataRes.isPending || approveDataRes.isPending) return
+    if (badgeDataRes.isError || approveDataRes.isError) {
+      console.log('error!!')
+      return
+    }
 
-      setBadgeData(data)
-      return data
-    })()
-  }, [])
+    if (badgeDataRes.isSuccess) {
+      setBadgeData(badgeDataRes.data)
+    }
+
+    if (approveDataRes.isSuccess) {
+      setApproveData(approveDataRes.data)
+    }
+  }, [res])
+
+  console.log(badgeData, approveData)
 
   return (
-    <>
+    <div className="flex flex-col gap-[10rem]">
       {badgeData && (
         <AdminTable
           title="뱃지 요청 목록"
@@ -35,20 +66,35 @@ export default function BadgeIssuePage() {
               head: '뱃지 승인',
               data: '승인',
               action: ApproveBadge,
+              refresh: () =>
+                queryClient.invalidateQueries({ queryKey: ['badgeData', 'issue'] }),
             },
-            { head: '뱃지 거절', data: '거절', action: RejectBadge },
+            {
+              head: '뱃지 거절',
+              data: '거절',
+              action: RejectBadge,
+              refresh: () =>
+                queryClient.invalidateQueries({ queryKey: ['badgeData', 'approve'] }),
+            },
           ]}
         />
       )}
-    </>
+      {approveData && (
+        <AdminTable
+          title="뱃지 승인 목록"
+          des="관리자가 승인한 뱃지 리스트입니다."
+          data={approveData}
+          addtionalColumn={[
+            {
+              head: '뱃지 승인 취소',
+              data: '승인 취소',
+              action: () => {
+                console.log('승인 취소')
+              },
+            },
+          ]}
+        />
+      )}
+    </div>
   )
-}
-
-async function ApproveBadge(id: string) {
-  const res = await axiosWithAuth.get(`/admin/verify/req/${id}`)
-  console.log(res)
-}
-
-async function RejectBadge(id: string) {
-  console.log('bye')
 }
