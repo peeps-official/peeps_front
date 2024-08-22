d14347ef1a472a513ee5c229556b219abc270053
Eom Hyun Jun <zzp1318@gmail.com>
2024-08-22 14:02:35 +0900

    👽️ fixed(mypage) API 데이터값 변경에 따른 코드 수정 - changeKey 이용해 서버와 다른 키값을 수정했습니다.

--- src/common/api/mypage.ts
+++ src/common/api/mypage.ts
@@ -18,9 +18,20 @@ export async function getLoginUserData() {
 
 /**
  * @description owner 유저 프로필 데이터를 가져옵니다.
+ * [ TODO ]
+ * changeKey를 이용해서 하나하나 키값을 변경하는 부분이
+ * 서버에 관련 데이터(지금은 UserProfile_T 데이터)를 보내는 코드와 의존성이 생겨 나중에 문제가 될 수 있음.
+ * 현재는 editOwnerProfile 함수에서만 사용하므로 문제가 없지만
+ * 추후 서버에 유저 데이터를 보내는 함수가 늘어날 경우 수정이 필요함.
  */
-export async function getOwnerUserData(user_seq: string) {
+export async function getOwnerUserData(user_seq: string): Promise<UserProfile_T> {
   const { data } = await axiosWithAuth.get(`/${user_seq}/profile`)
+
+  changeKey(data, 'id', 'user_seq')
+  changeKey(data, 'nickname', 'user_nickname')
+  changeKey(data, 'info', 'profileMessage')
+  changeKey(data, 'backgroundImage', 'user_bg_img')
+  changeKey(data, 'profileImage', 'user_profile_img')
 
   return data
 }
@@ -32,6 +43,7 @@ export async function editOwnerProfile(data: UserProfile_T) {
   const { user_seq, user_nickname, user_profile_img, user_bg_img, profileMessage } = data
 
   return axiosWithAuth.patch(`/${user_seq}/profile`, {
+    id: user_seq,
     nickname: user_nickname,
     profileImage: user_profile_img,
     backgroundImage: user_bg_img,
