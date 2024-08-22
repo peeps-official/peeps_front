75c00466fdec4a3e18374cc30882fa8f9f3bfb11
Eom Hyun Jun <zzp1318@gmail.com>
2024-08-22 15:24:25 +0900

    ✨ feat+refact(UserProfile): 프로필 사진 수정 기능 추가 및 디자인 수정 및 코드 리팩토링 - 프로필 사진 수정 기능 추가했습니다. - 프로필 디자인 수정했습니다. - Profile(image,info) 분리

---
+++ src/app/(userPage)/[user_seq]/_components/Profile/Items/ProfileImage.tsx
@@ -0,0 +1,51 @@
+'use client'
+import { editOwnerProfile } from '@/src/common/api/mypage'
+import { UserProfile_T } from '@/src/common/types/user'
+import NextImg from '@/src/common/utils/NextImg'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { FaCamera } from 'react-icons/fa'
+
+type ProfileImageProps = {
+  alt: string
+  src: string | null
+  isOwner: boolean
+  ownerData: UserProfile_T
+}
+
+export default function ProfileImage({ alt, src, isOwner, ownerData }: ProfileImageProps) {
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: async (data: UserProfile_T) => {
+      return editOwnerProfile(data)
+    },
+    onSuccess: (data) => {
+      console.log(data)
+      queryClient.invalidateQueries({ queryKey: ['owner', 'userPage'] })
+    },
+  })
+
+  function handleChangeProfileImg() {
+    const url = window.prompt('프로필 이미지 URL을 입력해주세요.')
+    mutate({ ...ownerData, user_profile_img: url })
+  }
+
+  return (
+    <>
+      <div className="absolute top-[-50px] h-[168px] w-[168px] rounded-full bg-[white] p-[4px]">
+        <div className="h-full w-full cursor-pointer overflow-hidden rounded-full object-cover">
+          <NextImg alt={alt} src={src ?? '/images/profile/profile_default.png'} />
+        </div>
+        {isOwner && (
+          <button
+            className="absolute bottom-[15px] right-[10px] z-[100] flex h-[36px] w-[36px] items-center justify-center overflow-hidden rounded-full bg-[#E4E6EB] p-[9px] hover:bg-slate-300"
+            onClick={handleChangeProfileImg}
+          >
+            <FaCamera className="h-full w-full" />
+          </button>
+        )}
+      </div>
+      <div className="w-[174px]" />
+    </>
+  )
+}
