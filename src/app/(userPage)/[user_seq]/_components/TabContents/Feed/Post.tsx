ff5112cb93bc33a687019630ed263dfbce4be69a
Eom Hyun Jun <zzp1318@gmail.com>
2024-10-04 15:37:50 +0900

    💄style(post): 포스트 이미지 뷰어 변경

--- src/app/(userPage)/[user_seq]/_components/TabContents/Feed/Post.tsx
+++ src/app/(userPage)/[user_seq]/_components/TabContents/Feed/Post.tsx
@@ -48,25 +48,6 @@ export default function Post({ post }: Props) {
   const { description, image, create_date, comments, boardLike } = post
   const srcs: string[] = JSON.parse(image)
 
-  const settings = {
-    customPaging: function (i: number) {
-      return (
-        <a>
-          <img src={srcs[i]} className="object-cover" />
-        </a>
-      )
-    },
-    dots: true,
-    dotsClass: 'slick-thumb',
-    infinite: true,
-    speed: 500,
-
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-  }
-
   return (
     <div className="w-full rounded-lg bg-white p-4 shadow-popupBox">
       <div className="flex items-start space-x-4">
@@ -89,7 +70,7 @@ export default function Post({ post }: Props) {
               />
               {isOption && (
                 <>
-                  <div className={`z-pop absolute right-0 top-8 w-56 rounded-lg bg-white p-4 shadow-popupBox`}>
+                  <div className={`z-pop absolute right-0 top-8 w-56 rounded-lg bg-white p-[16px] shadow-popupBox`}>
                     <button className="kr-bold-18 flex w-full rounded-md p-2 text-center text-gray-600 hover:bg-[#eee]">
                       <span>수정</span>
                       <div className="h-5 w-5"></div>
@@ -113,71 +94,21 @@ export default function Post({ post }: Props) {
             </div>
           </div>
           <p className="mt-4 text-gray-700">{description}</p>
-          <div className="mb-8 h-fit w-[500px] overflow-hidden">
-            <Slider {...settings}>
-              {srcs &&
-                srcs.map((src) => (
-                  <div
-                    key={src}
-                    id="image"
-                    className="mb-2 mt-4 h-[300px] w-[300px] max-w-[500px] overflow-hidden rounded-lg"
-                  >
-                    <NextImg src={src} alt="feed img" />
-                  </div>
-                ))}
-            </Slider>
+          <div className="maxWidthWithoutPadding scrollbar-hide mb-8 mt-4 flex h-fit overflow-x-auto">
+            {srcs &&
+              srcs.map((src) => (
+                <div key={src} id="image" className="mb-2 shrink-0 pr-[6px]">
+                  <img
+                    src={src}
+                    alt="feed img"
+                    className="max-h-[400px] max-w-full overflow-hidden rounded-lg border-[1px] border-solid border-black/40 object-cover"
+                  />
+                </div>
+              ))}
           </div>
-          <InteractionButtons comments={comments} boardLike={boardLike} />
         </div>
       </div>
     </div>
-  )
-}
-
-const IconButton = ({ icon, label }: any) => {
-  return (
-    <button className="flex items-center space-x-1 text-gray-600 hover:text-blue-500">
-      <span className="h-[15px] w-[15px]">{icon}</span>
-      <span>{label}</span>
-    </button>
-  )
-}
-
-type InteractionButtonsProps = {
-  comments: number
-  boardLike: number
-}
-
-const InteractionButtons = ({ comments, boardLike }: InteractionButtonsProps) => {
-  return (
-    <div className="mt-4 flex items-center justify-between">
-      <div className="flex space-x-4">
-        <IconButton icon={<GoHeartFill style={{ width: '100%', height: '100%' }} />} label={boardLike.toString()} />
-        <IconButton icon={<FaComment style={{ width: '100%', height: '100%' }} />} label={comments.toString()} />
-      </div>
-    </div>
-  )
-}
-
-function PrevArrow(props: any) {
-  const { className, style, onClick } = props
-  return (
-    <button
-      className={className}
-      style={{ ...style, left: '10px', zIndex: 1, fontSize: '50px', color: 'white' }}
-      onClick={onClick}
-    />
-  )
-}
-
-function NextArrow(props: any) {
-  const { className, style, onClick } = props
-  return (
-    <button
-      className={className}
-      style={{ ...style, right: '10px', zIndex: 1, fontSize: '50px', color: 'white' }}
-      onClick={onClick}
-    />
   )
 }
 
