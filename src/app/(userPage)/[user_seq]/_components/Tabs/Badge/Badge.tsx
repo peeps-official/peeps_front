df31c7b939f25a46d815eeece5b52d39793e45ac
Eom Hyun Jun <zzp1318@gmail.com>
2024-10-21 17:50:38 +0900

    ✨feat(Tabs/Badge): 뱃지 탭, 컨텐츠 추가(뱃지 리스트)

--- src/app/(userPage)/[user_seq]/_components/Tabs/Badge/Badge.tsx
+++ src/app/(userPage)/[user_seq]/_components/Tabs/Badge/Badge.tsx
@@ -1,5 +1,26 @@
 'use client'
 
+import { OwnerBadgeListAtom } from '@/src/common/recoil/ownerAtom'
+import { Badge_T } from '@/src/common/types/badge'
+import { useRecoilValue } from 'recoil'
+import BadgeItemContainer from '../InfoEdit/Badge/BadgeItemContainer'
+import { IsOwnerAtom } from '@/src/common/recoil/userHome'
+
 export default function Badge() {
-  return <>뱃지 종류별로 보여주기</>
+  const ownerBadgeList = useRecoilValue<Badge_T[]>(OwnerBadgeListAtom)
+  const isOwner = useRecoilValue<boolean>(IsOwnerAtom)
+
+  return (
+    <div className="flex flex-wrap gap-[1rem]">
+      {ownerBadgeList ? (
+        ownerBadgeList.map((badge) => (
+          <div>
+            <BadgeItemContainer item={badge} key={badge.bdg_id} isOwner={isOwner} />
+          </div>
+        ))
+      ) : (
+        <div>등록된 뱃지가 없습니다.</div>
+      )}
+    </div>
+  )
 }
