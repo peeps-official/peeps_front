59676fdca4c777c4a631d1f72e8e5ad4fe092590
Eom Hyun Jun <zzp1318@gmail.com>
2024-10-29 23:34:14 +0900

    text(MainPage): 메인페이지 문구 수정

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -1,12 +1,12 @@
-'use client';
+'use client'
 
 export default function HeroSection() {
   return (
-    <section className="relative flex flex-col items-center justify-center h-screen bg-white">
+    <section className="relative flex h-screen flex-col items-center justify-center bg-white">
       <div className="relative z-10 text-center">
-        <h1 className="text-5xl font-bold text-black">친구들과 함께하는 더 나은 장소</h1>
-        <p className="mt-4 text-lg text-gray-500">인증, 써클, 채팅으로 어디서든 친구들과 소통해보세요.</p>
+        <h1 className="text-5xl font-bold text-black">신뢰를 쌓는 더 나은 장소</h1>
+        <p className="mt-4 text-lg text-gray-500">인증된 사람들과 소통하고 개성있는 나를 표현해보세요.</p>
       </div>
     </section>
-  );
+  )
 }
