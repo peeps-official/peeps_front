c23e78d7b6b6e38f14a95d3b4965ec6f0875c258
Eom Hyun Jun <zzp1318@gmail.com>
2024-11-04 00:45:37 +0900

    💄style(mainPage): 메인페이지 문구 수정

--- src/app/_components/Category.tsx
+++ src/app/_components/Category.tsx
@@ -5,26 +5,31 @@ import NextImg from '@/src/common/utils/NextImg'
 export default function CategorySection() {
   return (
     <section className="bg-white py-16">
-      <div className="container mx-auto px-6">
-        <h2 className="text-center text-3xl font-bold text-gray-800">어떤 일을 하든, PEEPS와 함께</h2>
+      <div className="container mx-auto px-6 leading-[140%]">
+        <h2 className="text-center text-3xl font-bold text-gray-800">PEEPS와 함께, 믿을 수 있는 만남</h2>
 
         <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           <div className="rounded-lg bg-white p-6 shadow-md">
             {/* <NextImg alt="Authenticate" src="/images/icons/Auth.png" styles="w-12 h-12" /> */}
-            <h3 className="mt-4 text-xl font-semibold">인증</h3>
-            <p className="mt-2 text-gray-600">인증을 통해서 나를 보여주세요.</p>
+            <h3 className="mt-4 text-xl font-semibold">다양한 인증 방법</h3>
+            <p className="mt-2 text-gray-600">이메일, 로그인, 서류, 블록체인 다양한 방법으로 인증할 수 있어요</p>
           </div>
 
           <div className="rounded-lg bg-white p-6 shadow-md">
             {/* <NextImg alt="Circle" src="/images/icons/Cicle.png" styles="w-12 h-12" /> */}
-            <h3 className="mt-4 text-xl font-semibold">써클</h3>
-            <p className="mt-2 text-gray-600">인증 키워드를 중심으로 사람들과의 모임을 만드세요.</p>
+            <h3 className="mt-4 text-xl font-semibold">민감 정보 숨기기</h3>
+            <p className="mt-2 text-gray-600">
+              인증 후에는 뱃지가 생성돼요. 뱃지를 통해서 인증 정보를 나타낼 수 있고, 민감한 정보는 숨길 수 있어요.
+            </p>
           </div>
 
           <div className="rounded-lg bg-white p-6 shadow-md">
             {/* <NextImg alt="Chat" src="/images/icons/chat.png" styles="w-12 h-12" /> */}
-            <h3 className="mt-4 text-xl font-semibold">채팅</h3>
-            <p className="mt-2 text-gray-600">실시간 채팅과 함께 친구들과 대화하세요.</p>
+            <h3 className="mt-4 text-xl font-semibold">연결되는 새로운 방법, 써클</h3>
+            <p className="mt-2 text-gray-600">
+              뱃지를 중심으로 써클이 생성돼요. 상대에 대한 의심없이, 동일한 뱃지를 가진 사람들끼리 자유롭게 이야기할 수
+              있어요.
+            </p>
           </div>
         </div>
       </div>
--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -4,8 +4,9 @@ export default function HeroSection() {
   return (
     <section className="relative flex h-screen flex-col items-center justify-center bg-white">
       <div className="relative z-10 text-center">
-        <h1 className="text-5xl font-bold text-black">신뢰를 쌓는 더 나은 장소</h1>
-        <p className="mt-4 text-lg text-gray-500">인증된 사람들과 소통하고 개성있는 나를 표현해보세요.</p>
+        <h1 className="text-4xl font-bold text-gray-600">믿을 수 없는 사람들을 위한</h1>
+        <h1 className="text-5xl font-bold leading-[180%] text-black">인증 기반 소셜 네트워크 서비스</h1>
+        <p className="mt-6 text-lg text-gray-500">인증을 통해 나를 나타내고, 인증된 다른 사람들과 연결될 수 있어요.</p>
       </div>
     </section>
   )
