e9d92b532bbf8e74e0b73e12b911b2b70d9f2220
Eom Hyun Jun <zzp1318@gmail.com>
2024-11-07 00:49:53 +0900

    ✨feat(auth): 로그인 인증 추가

--- src/app/(userPage)/[user_seq]/_components/Tabs/InfoEdit/AddBadge/Login/GetLoginAuth.tsx
+++ src/app/(userPage)/[user_seq]/_components/Tabs/InfoEdit/AddBadge/Login/GetLoginAuth.tsx
@@ -1,12 +1,46 @@
 'use client'
 import { GoLock } from 'react-icons/go'
 
+import {
+  googleImgUrl,
+  googleUrl,
+  kakaoImgUrl,
+  kakaoUrl,
+  naverImgUrl,
+  naverUrl,
+  youtubeImgUrl,
+  youtubeUrl,
+} from '@/src/common/const/login'
+import NextImg from '@/src/common/utils/NextImg'
 import AddAuthContainer from '../AddAuthContainer'
 
 export default function GetLoginAuth() {
   return (
-    <AddAuthContainer title="로그인 인증" subTitle="로그인하여 인증을 받을 수 있어요" icon={<GoLock />}>
-      <div>로그인</div>
+    <AddAuthContainer title="로그인 인증" subTitle="로그인을 통하여 회원 정보를 인증받을 수 있어요." icon={<GoLock />}>
+      <a target="_blank" href={naverUrl} className="flex items-center gap-1">
+        <div className="h-5 w-5">
+          <NextImg src={naverImgUrl} alt="네이버" />
+        </div>
+        <p>네이버 로그인</p>
+      </a>
+      <a target="_blank" href={kakaoUrl} className="flex items-center gap-1">
+        <div className="h-5 w-5">
+          <NextImg src={kakaoImgUrl} alt="카카오" />
+        </div>
+        <p>카카오 로그인</p>
+      </a>
+      <a target="_blank" href={googleUrl} className="flex items-center gap-1">
+        <div className="h-5 w-5">
+          <NextImg src={googleImgUrl} alt="네이버" />
+        </div>
+        <p>구글 로그인</p>
+      </a>
+      <a target="_blank" href={youtubeUrl} className="flex items-center gap-1">
+        <div className="h-5 w-5">
+          <NextImg src={youtubeImgUrl} alt="네이버" />
+        </div>
+        <p>유튜브 로그인</p>
+      </a>
     </AddAuthContainer>
   )
 }
