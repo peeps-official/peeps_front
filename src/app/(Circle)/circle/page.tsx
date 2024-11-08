8b28b3a244b564450517ddb44df2bddeba804da2
Eom Hyun Jun <zzp1318@gmail.com>
2024-11-08 22:16:53 +0900

    ✨feat(Search): 써클 검색 기능 추가

--- src/app/(Circle)/circle/page.tsx
+++ src/app/(Circle)/circle/page.tsx
@@ -1,3 +1,55 @@
+'use client'
+
+import { axiosWithAuth } from '@/src/common/api/instance'
+import { Circle_T } from '@/src/common/types/circle'
+import NextImg from '@/src/common/utils/NextImg'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import DataWrapperForCircle from './_components/DataWrapperForCircle'
+
 export default function page() {
-  return <>아 이곳은 뱃지페이지</>
+  const [써클s정보, set써클s정보] = useState<Circle_T[]>([])
+
+  const searchParams = useSearchParams()
+  const search = searchParams.get('name')
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (search) searchCircle(search)
+  }, [search])
+
+  const { mutate: searchCircle } = useMutation({
+    mutationFn: async (data: string) => await axiosWithAuth.get(`/circle?name=${data}`),
+    onSuccess: (variables) => {
+      set써클s정보(variables.data)
+    },
+    onError: (error) => {
+      console.error('뱃지 검색에 실패했습니다.')
+      console.error(error)
+    },
+  })
+
+  console.log(써클s정보)
+
+  return (
+    <DataWrapperForCircle>
+      <div className="p-10">
+        {써클s정보.map((circle) => (
+          <button onClick={() => router.push(`circle/${circle.badge.name}`)} key={circle.sep_id}>
+            <div className="h-40 w-40 overflow-hidden rounded-lg shadow-md hover:shadow-xl">
+              <NextImg src={circle.badge.image ?? '/images/profile/profile.svg'} alt="user profile" />
+            </div>
+            <div className="w-40 px-2">
+              <p className="kr-bold-14 mt-2 w-full text-left">{circle.badge.name}</p>
+              <p className="kr-regular-12 overflow-hidden truncate text-nowrap text-left text-[#999]">
+                {circle.badge.description}
+              </p>
+            </div>
+          </button>
+        ))}
+      </div>
+    </DataWrapperForCircle>
+  )
 }
--- src/app/(userPage)/[user_seq]/_components/Header/GlobalHeader.tsx
+++ src/app/(userPage)/[user_seq]/_components/Header/GlobalHeader.tsx
@@ -1,47 +1,60 @@
 'use client'
-import { useCallback, useState, ChangeEvent } from 'react'
 
-import { useRecoilValue } from 'recoil'
+import Button from '@/src/common/components/Btn/Button'
 import { LogedInUserReqDataAtom } from '@/src/common/recoil/userAtom'
+import { LoginUserData_T } from '@/src/common/types/user'
 import NextImg from '@/src/common/utils/NextImg'
 import Link from 'next/link'
-import { LoginUserData_T } from '@/src/common/types/user'
-import Button from '@/src/common/components/Btn/Button'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import { useRecoilValue } from 'recoil'
 
 type GlobalHeaderProps = {
   onToggleSidebar: () => void
 }
 
 export default function GlobalHeader({ onToggleSidebar }: GlobalHeaderProps) {
+  const [검색, set검색] = useState<string>('')
   const { user_data: logedInUserData } = useRecoilValue<LoginUserData_T>(LogedInUserReqDataAtom)
-  const router = useRouter()
-  const [search, setSearch] = useState('')
 
-  const handleSearchValue = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value)
-    },
-    [search],
-  )
+  const searchParams = useSearchParams()
+  const search = searchParams.get('name')
+
+  const router = useRouter()
 
   const isUserLogedIn = logedInUserData.user_seq === '' ? false : true
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.value)
+    if (e.target.value) {
+      set검색(e.target.value)
+    } else {
+      set검색('')
+    }
+  }
+
+  useEffect(() => {
+    if (search) set검색(search)
+  }, [])
 
   return (
     <div className="fixed top-0 z-header flex h-header w-full min-w-[847px] flex-1 items-center justify-end bg-white pl-[20px] pr-[20px]">
       {/* 검색 */}
-      {/* <div className="absolute left-1/2 flex h-[36px] w-[321px] translate-x-[-50%] items-center justify-center gap-[6px] rounded-3xs bg-whitesmoke-300 pl-[8px] pr-[20px]">
+      <div className="absolute left-1/2 flex h-[36px] w-[321px] translate-x-[-50%] items-center justify-center gap-[6px] rounded-3xs bg-whitesmoke-300 pl-[8px] pr-[20px]">
         <Link href={'/'} className="h-[20px] w-[20px] pt-[1px]">
           <NextImg alt="search icon" src="/images/header/search.svg" styles="object-cover cursor-pointer" />
         </Link>
         <input
           id="search"
           type="text"
-          placeholder="커뮤니티 검색해보기"
+          defaultValue={search ?? ''}
+          placeholder="써클 검색해보기"
           className="h-full w-full flex-1 text-[15px] font-[400] leading-[1.2]"
-          onChange={handleSearchValue}
+          onChange={handleInput}
         />
-      </div> */}
+        {검색 && 검색.length > 0 && <button onClick={() => router.push(`/circle?name=${검색}`)}>검색</button>}
+      </div>
 
       {/* 프로필 & 버튼 */}
       <div className="flex items-center gap-[10px]">
