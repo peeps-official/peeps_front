b29d6ef7e4b6a3f3f381bdcfeb349838a633f4b0
Eom Hyun Jun <zzp1318@gmail.com>
2024-11-11 21:53:49 +0900

    🔊log(middlewar): admin 페이지 넘어가는 에러로 인한 로그 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import { axiosWithAuth } from './common/api/instance'
 export async function middleware(request: NextRequest) {
   let accessToken = request.cookies.get('loginToken')?.value
 
+  console.log('accessToken:', accessToken)
+  console.log('is Path Admin: ', request.nextUrl.pathname.startsWith('/admin'))
   // admin
   if (request.nextUrl.pathname.startsWith('/admin')) {
     // 로그인이 안되어있으면 로그인 페이지로 리다이렉트
@@ -11,12 +13,19 @@ export async function middleware(request: NextRequest) {
 
     // 권한이 없으면 로그인 페이지로 리다이렉트
     try {
-      await axiosWithAuth.get('/admin', {
-        headers: {
-          cookie: `loginToken=${accessToken}`,
-        },
-      })
+      console.log('try axiosWithAuth.get')
+      axiosWithAuth
+        .get('/admin', {
+          headers: {
+            cookie: `loginToken=${accessToken}`,
+          },
+        })
+        .then((res) => {
+          return NextResponse.next()
+        })
     } catch (err) {
+      console.log('catch axiosWithAuth.get')
+      console.error(err)
       return NextResponse.redirect(new URL('/login', request.url))
     }
   }
