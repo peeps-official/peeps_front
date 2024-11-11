fb1a04bd8d7204786a72e5d7f69ffcb6e9e037fa
Eom Hyun Jun <zzp1318@gmail.com>
2024-11-11 11:05:28 +0900

    ✨feat(admin): 권한 없으면 admin 페이지 접근 middleware 통해서 막도록 수정

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { axiosWithAuth } from './common/api/instance'
+
+export async function middleware(request: NextRequest) {
+  let accessToken = request.cookies.get('loginToken')?.value
+
+  console.log(request.nextUrl.pathname.startsWith('/admin'))
+  // admin
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    // 로그인이 안되어있으면 로그인 페이지로 리다이렉트
+    if (!accessToken) return NextResponse.redirect(new URL('/login', request.url))
+
+    // 권한이 없으면 로그인 페이지로 리다이렉트
+    try {
+      await axiosWithAuth.get('/admin', {
+        headers: {
+          cookie: `loginToken=${accessToken}`,
+        },
+      })
+    } catch (err) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
+
+  console.log('middleware 끝')
+}
+
+export const config = {
+  matcher: '/admin/:path*',
+}
